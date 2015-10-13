e71e0ddd19110df0f33fbf60a34145903071b88a
Niccolo <ncorsini@users.noreply.github.com>
2015-10-13 18:48:16 +0100

    update

--- Sensors/main.cpp
+++ Sensors/main.cpp
@@ -4,11 +4,16 @@
 #include<stdio.h>
 #include<thread>
 #include "/home/pi/Sonodot/trilat/tsi.cpp"
+#include "/home/pi/Sonodot/Kalman/Kalman_3D.cpp"
 #define PIN 29
 int main()
 {
  setup();
  int code=5,i=0;
+ 
+ double data_arr[3][100];
+ double pos_arr[3][2];
+ 
  RCSwitch mySwitch = RCSwitch();
  mySwitch.enableTransmit(PIN);
  receiver receiver1(1),receiver2(2),receiver3(3);
@@ -29,6 +34,22 @@ int main()
  t3.join();
  std::cout<<receiver1.distance<<"  "<<receiver2.distance<<"  "<<receiver3.distance<<"  "<<i<<std::endl;
  calculate(receiver1.distance,receiver2.distance,receiver3.distance);
+ //for(i=0;i<3;i++){
+ //  for(j=0;j<2;j++){
+     data_arr[0][i]=receiver1.distance;  //x
+     data_arr[1][i]=receiver2.distance;  //y
+     data_arr[2][i]=receiver3.distance;  //z
+
+     pos_arr[0][0]= data_arr[0][i-1] //x
+     pos_arr[1][0]= data_arr[1][i-1] //y
+     pos_arr[2][0]= data_arr[2][i-1] //z
+     pos_arr[0][1]= data_arr[0][i]   //x
+     pos_arr[1][1]= data_arr[1][i]   //y
+     pos_arr[2][1]= data_arr[2][i]   //z
+
+  kalman(pos_arr); 
+     
+     
  delay(50);
  }
  return 0;
