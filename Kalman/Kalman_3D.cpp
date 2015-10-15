2a349deb5b581171d0663bd3e5507ba1d1b9d259
andreistock <andreihirjanu@gmail.com>
2015-10-15 18:45:43 +0000

    bla

--- Kalman/Kalman_3D.cpp
+++ Kalman/Kalman_3D.cpp
@@ -16,13 +16,13 @@ using namespace std;
 
 
 
-void kalman(double pos_arr[3][2]){
+void kalman(double pos_arr[3][3]){
 
 ofstream data;
 data.open ("data.txt");
 
 int i,j,k;
-int n=3, t=2;    //Define dimensions of state vector X 
+int n=3, t=4;    //Define dimensions of state vector X 
 
 
 // Define and initialise matrices
@@ -33,6 +33,20 @@ colvec Ytt(n),Xt(n),Xtt(n); //random noise vector
 b.randn(),c.randn();
 y.zeros(),x.zeros(),F.zeros(),B.zeros(),P.zeros(),Q.zeros(),K.zeros(),H.zeros(),R.zeros();
 I = eye<mat>(n,n);
+
+
+      y(0,1)=  pos_arr[0][0];
+      y(0,2)=  pos_arr[0][1];
+      y(0,3)=  pos_arr[0][2];
+      y(1,1)=  pos_arr[1][0];
+      y(1,2)=  pos_arr[1][1];
+      y(1,3)=  pos_arr[1][2];
+      y(2,1)=  pos_arr[2][0];
+      y(2,2)=  pos_arr[2][1];
+      y(2,3)=  pos_arr[2][2];
+  
+
+
 
 // Generate measurement vector Y with white noise. 
 
@@ -59,8 +73,8 @@ for (int i=0;i<t-1;i++) {
     K= P*trans(H)*inv(H*P*trans(H)+R);
     x.col(i+1)=x.col(i+1)+K*(y.col(i+1)-H*(x.col(i+1)));
     P=(I-K*H)*P;
-    data << i << "  " << x(0,i) << "  " << x(1,i) << "  " << y(0,i) << "  " << y(1,i) << endl;
-
+//    data << i << "  " << x(0,i) << "  " << x(1,i) << "  " << y(0,i) << "  " << y(1,i) << endl;
+data << i+1 << "  " << x(0,i+1)  << "  " << y(0,i+1) << "  "  << endl;
 }
 
 
