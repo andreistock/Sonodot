0af51668561269d351a46b7b01d56aa83fe00146
andreistock <andreihirjanu@gmail.com>
2015-12-04 18:45:00 +0000

    updatiiing

--- emitter/emitter.cpp
+++ emitter/emitter.cpp
@@ -14,10 +14,10 @@ int main()
 
   int i=0;
   timespec tsleep{0};
-  tsleep.tv_nsec=50000000;
+  tsleep.tv_nsec=30000000;
   receiver receiver1(1),receiver2(2);
   receiver1.init();
-  receiver2.init();
+//  receiver2.init();
   mySwitch = RCSwitch();
   mySwitch.enableReceive(PIN);
   
@@ -32,23 +32,24 @@ int main()
       }
       else
       {
-        printf("received %i\n",mySwitch.getReceivedValue() );
+        printf("received %i \n",mySwitch.getReceivedValue() );
 //        delay(3);
         while(1) 
         {
-//          auto begin = std::chrono::high_resolution_clock::now();
+          auto begin = std::chrono::high_resolution_clock::now();
        ///////////////////////////////////////////////////////////////////
           std::thread t1(&receiver::dst,&receiver1);
+//          receiver1.dst();
           std::thread t2(&receiver::dst,&receiver2);
           t1.join();
           t2.join();
-//          i++;
+          i++;
       ///////////////////////////////////////////////////////////////////////////
-//          auto end = std::chrono::high_resolution_clock::now();
-//          duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count();
-          tsleep.tv_nsec=tsleep.tv_nsec-duration;
+          auto end = std::chrono::high_resolution_clock::now();
+          duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count();
+          tsleep.tv_nsec = 50000000-duration;
           nanosleep(&tsleep,NULL);
-//          if (i=20)
+//          if (i==20)
 //          { i=0;
 //            goto mylabel;
 //          }
@@ -56,11 +57,11 @@ int main()
         }
 //        mySwitch.resetAvailable();
       }
-       
-//        mySwitch.resetAvailable();
+        mylabel :
+        mySwitch.resetAvailable();
     }
-    mylabel :
-    mySwitch.resetAvailable();
+//    mylabel :
+//    mySwitch.resetAvailable();
   }
   return 0;
 }
